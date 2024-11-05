62a9c25a4b3b732c389b859f5fe304fc9d507556
Takashi Sugimoto <succi0303@gmail.com>
2024-11-05 17:53:49 +0900

    Chapter3 1-3

---
+++ chapter3/1_langchain.py
@@ -0,0 +1,15 @@
+from langchain_aws import ChatBedrock
+from langchain_core.messages import HumanMessage, SystemMessage
+
+chat = ChatBedrock(
+    model_id="anthropic.claude-3-sonnet-20240229-v1:0",
+    model_kwargs={"max_tokens": 1000},
+)
+
+messages = [
+    SystemMessage(content="あなたのタスクはユーザーの質問に明確に答えることです。"),
+    HumanMessage(content="空が青いのはなぜですか？"),
+]
+
+response = chat.invoke(messages)
+print(response.content)
